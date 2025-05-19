146fb97c3ea54ff96b329478d3bde256057b51dd
Kanimod <0808kanimod@gmail.com>
2025-05-19 18:58:46 +0200

    Działa pop up window logowania!!!! brak linku przekierunkowania po wypelnieniu danymi :3

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function showLogin() {
+    document.getElementById("login-container").className = "shown";
+}
+
+function hideLogin() {
+    document.getElementById("login-container").className = "hidden";
+}
