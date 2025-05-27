bcf9a366edec74bcf604fa10f92c3cce5070b491
hubertus1512 <gudalewskihubert@gmail.com>
2025-05-27 19:22:47 +0200

    wyszukiwanie, rozowuj trybu ciemnego

--- skrypt.js
+++ skrypt.js
@@ -6,23 +6,60 @@ const showLogin = () => {
     document.getElementById("login-container").className = "hidden";
   }  
 
+
 const toggleButton = document.getElementById('tryb');
-const body = document.body;
+const cialo = document.getElementById('cialo');
+const glowa = document.getElementById('glowa');
+
+
+
+
 
 const zapiszTryb = localStorage.getItem('them');
 
 if (zapiszTryb){
-  body.classList.add(zapiszTryb)
+  cialo.classList.add(zapiszTryb);
+  glowa.classList.add(zapiszTryb);
 } else{
-  body.classList.add('jasno');
-}
+  cialo.classList.add('jasno');
+  glowa.classList.add('jasno');
+};
 
 toggleButton.addEventListener('click', () => {
-    if (body.classList.contains('jasno')) {
-        body.classList.replace('jasno', 'ciemno');
-        localStorage.setItem('them', 'ciemno')
+    if (cialo.classList.contains('jasno')) {
+        cialo.classList.replace('jasno', 'ciemno');
+        glowa.classList.replace('jasno', 'ciemno');
+        localStorage.setItem('them', 'ciemno');
     } else {
-        body.classList.replace('ciemno', 'jasno');
-        localStorage.setItem('them', 'jasno')
+        cialo.classList.replace('ciemno', 'jasno');
+        glowa.classList.replace('ciemno', 'jasno');
+        localStorage.setItem('them', 'jasno');
     }
 });
+
+document.getElementById("przycisk_wy").addEventListener("click", function(){
+  const zapyt = document.getElementById("wyszukiwanie").value.toLocaleLowerCase();
+  const sekcje = document.getElementById("gryy").querySelectorAll("h2");
+  let zanelzione = false;
+
+  sekcje.forEach(function(sekcja){
+    if (sekcja.textContent.toLocaleLowerCase().includes(zapyt)){
+      sekcja.scrollIntoView({ behavior: "smooth", block: "center" })
+      zanelzione = true;
+    }
+  });
+
+  if (!zanelzione){
+    alert("nie znaleziono")
+  }
+
+  document.getElementById("wyszukiwanie").value = "";
+});
+
+
+
+
+
+
+
+
